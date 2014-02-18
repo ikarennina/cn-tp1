443414d4d99172af040ad50d49d049bd3a54919d
Izabela Karennina Travizani Maffra <bela@Izabelas-MacBook-Air.local>
2014-02-18 19:39:32 -0300

    Corrected bug in degree dist and changed files structure

---
+++ src/main.py
@@ -0,0 +1,51 @@
+"""A module to perform metric calculations in a graph."""
+
+import networkx as nx
+
+def get_degree_dist(graph, undirected=True):
+    """Plot degree distribution of graph.
+
+    Args:
+        graph: a networkx graph object
+        undirected: boolean indicating if graph is undirected or not (True by
+            default)
+
+    Returns:
+        degree_dist: A dictionary with degree as keys and count(degree) as
+            value, if graph is undirected.
+        degree_dist, in_degree_dist, out_degree_dist: A tuple containing the
+            cummulative degree distribution, the in-degree distribution and the
+            out-degree distribution, if graph is directed.
+    """
+    degrees_list = graph.degree().values()
+    degree_dist = {v: degrees_list.count(v) for v in degrees_list}
+    if undirected:
+        return degree_dist
+    in_degrees_list = graph.in_degree().values()
+    in_degree_dist = {v: in_degrees_list.count(v) for v in in_degrees_list}
+    out_degrees_list = graph.out_degree().values()
+    out_degree_dist = {v: out_degrees_list.count(v) for v in out_degrees_list}
+    return degree_dist, in_degree_dist, out_degree_dist
+
+
+def main():
+    """"Main function of the module."""
+    #read_input()
+    graph = nx.fast_gnp_random_graph(5, .3)
+    print graph.nodes()
+    get_degree_dist(graph)
+   # return
+   # plot_clust_coeff()
+   # plot_connected_comp_dist()
+   # plot_neigh_overlap_dist()
+   # plot_distance_dist()
+   # plot_edges_betweeness()
+   # plot_nodes_betweeness()
+   # find_bridges()
+   # plot_graph()
+
+
+
+
+if __name__ == '__main__':
+    main()
