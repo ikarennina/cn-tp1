2d216c89a89e15cb74caccfe77560c9378a51486
Izabela Karennina Travizani Maffra <bela@Izabelas-MacBook-Air.local>
2014-02-18 21:13:06 -0300

    Added function to calculate local clust. coeff.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,34 @@
 """A module to perform metric calculations in a graph."""
 
+from itertools import combinations
+from itertools import permutations
+
 import networkx as nx
+
+def get_local_cluster_coeff(graph, node_id):
+    """Calculate local cluster coefficient.
+    
+    Args:
+        graph: a networkx graph object
+        node_id: the id of a node in the graph
+
+    Returns:
+        cluster_coeff: The cluster coefficient of the node, calculated as the
+            probability of two random neighbors of the node being connected.
+    """
+    neighbors = graph.neighbors(node_id)
+    #disconsider self loops
+    if node_id in neighbors:
+        neighbors.remove(node_id)
+    count = 0
+    candidates = permutations(neighbors, 2) if graph.is_directed() else \
+        combinations(neighbors, 2)
+    for edge in candidates:
+        if graph.has_edge(x, y)
+            count += 1
+    cc = count/(len(neighbors) * (len(neighbors) - 1))
+    return cc if graph.is_directed() else 2 * cc
+
 
 def get_degree_dist(graph):
     """Plot degree distribution of graph.
@@ -17,7 +45,7 @@ def get_degree_dist(graph):
     """
     degrees_list = graph.degree().values()
     degree_dist = {v: degrees_list.count(v) for v in degrees_list}
-    if not graph.is_directed:
+    if not graph.is_directed():
         return degree_dist
     in_degrees_list = graph.in_degree().values()
     in_degree_dist = {v: in_degrees_list.count(v) for v in in_degrees_list}
@@ -28,11 +56,8 @@ def get_degree_dist(graph):
 
 def main():
     """"Main function of the module."""
-    #read_input()
-    graph = nx.fast_gnp_random_graph(5, .3)
-    print graph.nodes()
-    get_degree_dist(graph)
-   # return
+   # read_input()
+   # plot_degree_dist(graph)
    # plot_clust_coeff()
    # plot_connected_comp_dist()
    # plot_neigh_overlap_dist()
@@ -46,4 +71,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    # main()
--- test/test_metrics.py
+++ test/test_metrics.py
@@ -11,6 +11,7 @@ class testDegreeDistribution(unittest.TestCase):
     """Test the fet_degree_dist function."""
     
     def test_directed(self):
+        """Test with directed graph."""
         graph = nx.DiGraph([(0, 0), (0, 1), (1, 2), (2, 3), (3, 2), (3, 4), (4,
             0), (4, 2)])
         result = metrics.get_degree_dist(graph)
@@ -21,12 +22,12 @@ class testDegreeDistribution(unittest.TestCase):
                 result)
     
     def test_undirected(self):
-        pass
-
-
-
-
-
+        """Test with undirected graph."""
+        graph = nx.Graph([(0, 1), (0, 2), (0, 4), (1, 2), (1, 3), (2, 3), (3,
+            4), (4, 0)])
+        result = metrics.get_degree_dist(graph)
+        degree_dist = {2: 1, 3: 4}
+        self.assertEqual(degree_dist, result)
 
 
 if __name__ == '__main__':
